2cf8fe30febaf343f796578db7d5348ec3bea449
Lucas Heber <lucas.heber07@gmail.com>
2020-03-13 22:44:52 -0300

    [add] post controller, [add] create viewer

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,22 @@
+<form action="{{route('posts.store')}}" method="post">
+    <div class="form-group">
+        <label for="title">Titulo</label>
+        <input type="text" name="title" id="title" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="description">Descrição</label>
+        <input id="description" class="form-control" type="text" name="description">
+    </div>
+
+    <div class="form-group">
+        <label for="content">Conteúdo</label>
+        <input id="content" class="form-control" type="text" name="content">
+    </div>
+    <div class="form-group">
+        <label for="slug">Slug</label>
+        <input id="slug" class="form-control" type="text" name="slug">
+    </div>
+
+    <button class="btn btn-lg btn-success">Criar</button>
+</form>
