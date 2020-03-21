ede45529b5ed38bb611b582a2eaa2a4ca2946b24
Lucas Heber <lucas.heber07@gmail.com>
2020-03-21 10:57:50 -0300

    [up] category controller

--- app/Category.php
+++ app/Category.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    
+
+    protected $fillable = ['name', 'description', 'slug'];
+
     public function posts()
     {
         return $this->belongsToMany(Post::class, 'posts_categories');
---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Category;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /** @var Category */
+    private $category;
+
+    public function __construct(Category $category)
+    {
+        $this->$category = $category;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = $this->category->paginate(15);
+
+        return view('categories.index', compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        try {
+            $category = $this->category->create($data);
+            flash('Categoria criada com sucesso!')->success();
+            return redirect()->route('categories.index');
+        } catch (\Exception $e) {
+            $message = 'Erro ao criar categoria!';
+
+            if (env('APP_DEBUG')) {
+                $message = $e->getMessage();
+            }
+
+            flash($message)->warning();
+            return redirect()->back();
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        return view('categories.edit', compact('category'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        return redirect()->route('categories.show', ['category' => $category->id]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        $data = $request->all();
+
+        try {
+            $category->update($data);
+            flash('Categoria atualizada!')->success();
+            return redirect('categories.show', ['category' => $category->id]);
+        } catch (\Exception $e) {
+            $message = 'Error ao atualizar a categoria!';
+
+            if (env('APP_DEBUG')) {
+                $message = $e->getMessage();
+            }
+
+            flash($message)->warning();
+
+            return redirect()->back();
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        try {
+            $category->delete();
+            flash("Categoria '{$category->name}' removida!");
+
+            return redirect()->route('categories.index');
+        } catch (\Exception $e) {
+            $message = "Falha a remover a categoria '{$category->name}'.";
+
+            if (env('APP_DEBUG')) {
+                $message = $e->getMessage();
+            }
+
+            flash($message)->warning();
+            return redirect()->back();
+        }
+    }
+}
