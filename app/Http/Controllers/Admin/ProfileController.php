44eb83ae3565ab53835bc977aef8ac423e17ec2b
Lucas Heber <lucas.heber07@gmail.com>
2020-03-26 13:59:09 -0300

    [up] profile index

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\User;
+use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
 
     public function index()
     {
-        /** @var User */
+
         $user = auth()->user();
 
         if (!$user->profile()->count()) {
@@ -18,5 +19,39 @@ class ProfileController extends Controller
         }
 
         return view('profile.index', compact($user));
+    }
+
+    public function update(Request $request)
+    {
+        $userData = $request->get('user');
+        $profileData = $request->get('profile');
+
+        try {
+            if ($userData['password']) {
+                $userData['password'] = bcrypt($userData['password']);
+            } else {
+                unset($userData['password']);
+            }
+
+            /** @var User */
+            $user = auth()->user();
+
+            $user->update($userData);
+            $user->profile()->update($profileData);
+
+            flash('Perfil atualizado com sucesso!')->success();
+
+            return redirect()->route('profile.index');
+        } catch (\Throwable $th) {
+            $message = "Erro ao atualizar o perfil";
+
+            if (env('APP_DEBUG')) {
+                $message = $th->getMessage();
+            }
+
+            flash($message)->warning();
+
+            return redirect()->back();
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -27,9 +27,15 @@ Route::group(['middleware' => ['auth']], function () {
     Route::prefix('admin')->namespace('Admin')->group(function () {
         Route::resource('posts', 'PostController');
         Route::resource('categories', 'CategoryController');
-    });
 
+        Route::prefix('profile')->name('profile')->group(function () {
+            Route::get('/', 'ProfileController@index')->name('index');
+            Route::post('/', 'ProfileController@update')->name('update');
+        });
+    });
 });
+
+
 
 Auth::routes();
 
