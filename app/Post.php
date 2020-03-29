854dc7d43b872b54ffed56a20b99560e794c0726
Lucas Heber <lucas.heber07@gmail.com>
2020-03-29 11:44:57 -0300

    [add] thumb column

--- app/Post.php
+++ app/Post.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = [
-        'title', 'description', 'content', 'slug', 'is_active', 'user_id',
+        'title', 'description', 'content', 'slug', 'is_active', 'user_id', 'thumb'
     ];
 
     public function user()
--- database/migrations/2020_03_29_143721_alter_table_posts_add_column_thumb.php
+++ database/migrations/2020_03_29_143721_alter_table_posts_add_column_thumb.php
@@ -14,7 +14,7 @@ class AlterTablePostsAddColumnThumb extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->string('thumb')->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ class AlterTablePostsAddColumnThumb extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->dropColumn('thumb');
         });
     }
 }
