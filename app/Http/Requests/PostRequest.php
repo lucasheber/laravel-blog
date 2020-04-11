158e99c8cede4f21993ea74af407c779ce3d3ae6
Lucas Heber <lucas.heber07@gmail.com>
2020-04-11 10:12:17 -0300

    [add] postRequest validator

---
+++ app/Http/Requests/PostRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required',
+            'description' => 'required|min:20',
+            'content' => 'required',
+            'thumb' => 'required|image',
+            'categories' => 'required',
+        ];
+    }
+}
